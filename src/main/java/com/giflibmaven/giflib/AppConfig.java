f64728f5f02d96cc19709a2d7a43e253cf85b400
martinsir <double_pun@hotmail.com>
2017-04-11 17:51:44 +0200

    A war file that runs with java -jar 'FileName' reedit the AppConfig class.
    edited the pom file to java version 1.8 from 1.7
    
    added application.properties file

--- src/main/java/com/giflibmaven/giflib/AppConfig.java
+++ src/main/java/com/giflibmaven/giflib/AppConfig.java
@@ -2,25 +2,30 @@ package com.giflibmaven.giflib; /**
  * Created by Martin H on 10-04-2017.
  */
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
 
-public class AppConfig extends SpringBootServletInitializer {
+public class AppConfig extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(AppConfig.class);
+        return configureApplication(builder);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(AppConfig.class, args);
+        configureApplication(new SpringApplicationBuilder()).run(args);
+    }
+
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+        return builder.sources(AppConfig.class).bannerMode(Banner.Mode.OFF);
     }
 }
