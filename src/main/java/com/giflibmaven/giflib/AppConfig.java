69d4cd1394f5b5d26ecc3846708e87e9799c6796
martinsir <double_pun@hotmail.com>
2017-04-11 14:54:12 +0200

    A war file that runs with java -jar 'FileName' reedit the AppConfig class.

--- src/main/java/com/giflibmaven/giflib/AppConfig.java
+++ src/main/java/com/giflibmaven/giflib/AppConfig.java
@@ -5,20 +5,22 @@ package com.giflibmaven.giflib; /**
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
-//extends SpringBootServletInitializer
-public class AppConfig{
+
+public class AppConfig extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AppConfig.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AppConfig.class, args);
     }
-
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return builder.sources(Application.class);
-//    }
 }
